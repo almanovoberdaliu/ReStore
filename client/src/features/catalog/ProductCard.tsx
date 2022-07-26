29e4a58acf2a3e32c03ecdce836d99366deabb06
almanovoberdaliu <an51718@ubt-uni.net>
2022-07-26 01:03:08 +0100

    added a functionality that calculates the subtotal and the delivery

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import agent from "../../app/api/agent";
 import { Product } from "../../app/models/product"
+import { currencyFormat } from "../../app/util/util";
 import { UseStoreContext } from "../../context/StoreContext";
 
 interface Props {
@@ -41,7 +42,7 @@ export default function ProductCard({ product }: Props) {
             />
             <CardContent>
                 <Typography gutterBottom color='secondary' variant="h5" >
-                    ${(product.price / 100).toFixed(2)}
+                   {currencyFormat(product.price)}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                     {product.brand}
